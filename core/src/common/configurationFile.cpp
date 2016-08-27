cf4990c7cbb6204fe33828fe81b0ed4017724728
PaulVisscher <paul.pev@hotmail.com>
2016-08-27 17:51:54 +0200

    Debug specific case

--- core/src/common/configurationFile.cpp
+++ core/src/common/configurationFile.cpp
@@ -654,7 +654,10 @@ size_t ConfigurationFile::GetCommentPosition( const std::string &line )
         }
         else
         {
-            std::cout << cur << " " << countQ1 << " " << countQ2 << std::endl;
+            if ( cur == "#" )
+            {
+                std::cout << cur << " " << countQ1 << " " << countQ2 << std::endl;
+            }
 
             if ( cur == "#" && ( countQ1 & 0x01 ) == 0 && ( countQ2 & 0x01 ) == 0 )
             {
