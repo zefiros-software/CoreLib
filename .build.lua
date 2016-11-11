2f79c219f1273dd43d7e1023766b224c2928c512
PaulVisscher <paul.pev@hotmail.com>
2016-11-11 13:41:52 +0100

    Export program settings

--- .build.lua
+++ .build.lua
@@ -4,10 +4,16 @@ project "CoreLib"
     files "core/src/**.cpp"   
     kind "StaticLib"        
 
-    zpm.export [[
+    zpm.export(function()
         includedirs "core/include/"
         flags "C++14"
-        pic "On"     
+        pic "On" 
+
+        defines {
+            "PROGRAM_NAME="..zpm.setting( "PROGRAM_NAME" ),
+            "PROGRAM_COMPANY="..zpm.setting( "PROGRAM_COMPANY" ),
+            "PROGRAM_COPYRIGHT="..zpm.setting( "PROGRAM_COPYRIGHT" )
+        }    
 
         filter "system:not macosx"
             linkgroups "On"
@@ -19,7 +25,7 @@ project "CoreLib"
             }
 
         filter {}
-    ]]
+    end)
     
     zpm.uses {
         "Zefiros-Software/DocoptCpp",
