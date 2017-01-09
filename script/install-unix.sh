ee42e289c5ecee1b3dbe31fada81145a825d7479
PaulVisscher <paul.pev@hotmail.com>
2017-01-09 17:53:04 +0100

    Remove non compiling part

--- script/install-unix.sh
+++ script/install-unix.sh
@@ -11,9 +11,4 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
     # for coverage
     sudo pip install codecov
-else
-
-    sed -i -- 's%-Wl,--end-group%%' *.make
-    sed -i -- 's%-Wl,--start-group%%' *.make
-    sed -i -- 's%-L/usr/lib64%%' *.make
 fi
