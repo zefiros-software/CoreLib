4f4dfabe7d6eee639f5f1604e74439959c4f99e2
p.e.visscher <p.e.visscher@zefiros.eu>
2017-01-13 14:11:38 +0100

    Test various gcc versions

--- script/install-unix.sh
+++ script/install-unix.sh
@@ -1,13 +1,14 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -y
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-get update -y
-    sudo apt-get install gcc-6 g++-6 -y
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 60
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
+
+    sudo apt-get install gcc-${VERSION} g++-${VERSION} -y
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${VERSION} 60
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${VERSION} 60
     sudo update-alternatives --config gcc
     sudo update-alternatives --config g++
-    sudo apt-get install gcc-6-multilib g++-6-multilib
+    sudo apt-get install gcc-${VERSION}-multilib g++-${VERSION}-multilib
 
     # for coverage
     sudo pip install codecov
