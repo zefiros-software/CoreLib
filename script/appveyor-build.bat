b8df53bcdb96587a12e7f2541dd2c2e9bcf865d1
p.e.visscher <p.e.visscher@zefiros.eu>
2017-01-15 13:15:36 +0100

    Fix zpm builds

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -1,17 +1,15 @@
 
 if "%TYPE%" == "zpm" (    
-    echo 'Installing ZPM...'
     cd test
     
-    zpm install-package --allow-install --allow-module
-    zpm vs2015 --allow-install
+    zpm install-package --allow-install --allow-module || exit /b 1
+    zpm vs2015 --allow-install || exit /b 1
 
     msbuild zpm/CoreLib-ZPM.sln || exit /b 1
 
     test\bin\x86\core-zpm-test.exe || exit /b 1
 ) else (
-    echo 'Installing...'
-    zpm install-package --allow-install --allow-module  || exit /b 1
+    zpm install-package --allow-install --allow-module || exit /b 1
     zpm %VSTUD% --allow-install || exit /b 1
 
     msbuild core/CoreLib.sln /property:Configuration=Release /property:Platform=%PLAT% || exit /b 1
