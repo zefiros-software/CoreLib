369f25185e8100ff6050c00ac14da58d79535124
Koen Visscher <k.m.visscher@vu.nl>
2017-04-03 15:27:50 +0200

    small log update

--- core/src/manager/logManager.cpp
+++ core/src/manager/logManager.cpp
@@ -73,7 +73,8 @@ void LogManager::OnInit()
         File::Clear( logFile );
     }
 
-    Console::Initp( "Log file \"%s\" opened on '{:%Y-%m-%dT%H:%M:%SZ}'", logFile, *std::localtime( &t ) );
+    Console::Initp( "Log file location: %s", logFile );
+    Console::Initp( "Log file opened on '{:%Y-%m-%dT%H:%M:%SZ}'", *std::localtime( &t ) );
 
     Log( mStartupBuffer.str() );
     mStartupBuffer.clear();
